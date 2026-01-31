a51a9f3d02a6d7e927b77f2c178bb3b85ffa6cd3
Igor Malyarov <igor1309@gmail.com>
2026-01-31 14:41:50 +0300

    Adjust Jest globals cleanup config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
+  testEnvironmentOptions: {
+    globalsCleanup: 'off',
+  },
 };
--- jest.setup.js
+++ jest.setup.js
@@ -6,13 +6,3 @@ jest.mock('fs', () => require('memfs').fs);
 jest.mock('./scripts/services/ai.js', () => ({
   getAIEnrichment: jest.fn(),
 }));
-
-try {
-  Object.defineProperty(globalThis, 'localStorage', {
-    configurable: true,
-    value: undefined,
-    writable: true,
-  });
-} catch {
-  // Best-effort: if localStorage is non-configurable, leave as-is.
-}
